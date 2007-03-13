40fed0771c87f4ec8f4bc306e3826b76aef6b30d
Dan Wendlandt <danwent@gmail.com>
2007-03-13 03:58:21 +0000

    adding modified linux kernel list, and updated common.h
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/multissh/notarycode@9 4bf60a73-bb1f-0410-bc46-cb087b644473

--- common.h
+++ common.h
@@ -1,6 +1,9 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
+#include "util/list.h"
+#include "key.h"
+#include <assert.h>
 
 #define MAX_PACKET_LEN 1400
 #define NOTARY_SERVER_PORT 2222
@@ -11,8 +14,6 @@
 #define TYPE_PROBE_REQ 0x04
 #define TYPE_PROBE_REPLY 0x05
 
-#define SERVICE_SSH 0x01
-
 // all fields should be in network byte order 
 
 typedef struct {
@@ -21,15 +22,28 @@ typedef struct {
 	uint16_t service_type;
 	uint16_t service_port;
 	uint16_t name_len;
+	uint16_t total_len;
 } __attribute__ ((packed)) notary_header;
 
 
 #define SSH_KEYTYPE_DSA 0x1
 #define SSH_KEYTYPE_RSA 0x2
+#define SSH_KEYTYPE_RSA1 0x3
 
+// ssh-key data struture used in memory
 typedef struct {
-	uint8_t key_type;
+	struct list_head list;
+	Key* key;
+	uint32_t ip_addr;
+	int32_t time_observed;		
+} ssh_key_list;
+
+// ssh-key data structure used in packet replies
+typedef struct {
+	uint16_t key_type;
 	uint16_t key_len_bytes;
+	uint32_t ip_addr;
+	int32_t time_observed; 
 } __attribute__ ((packed)) ssh_key_info;
 
 #endif
