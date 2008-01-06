af3e86829d7112ff9e287f913c0234e5e0009048
Dan Wendlandt <danwent@gmail.com>
2008-01-06 03:07:59 +0000

    forgot file needed for scanning
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/multissh/notarycode@177 4bf60a73-bb1f-0410-bc46-cb087b644473

---
+++ common/keyscan_util.c
@@ -0,0 +1,49 @@
+
+
+
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/wait.h>
+
+#include "notary_util.h" 
+#include "net_util.h"
+#include "keyscan_util.h"
+
+// function used by spawned client processes to report resulting key back to
+// the main scanner process.  called from sshconnect.c
+void record_key(char *dns_name, uint32_t ip_addr, uint16_t port,
+                  uint8_t* digest, uint32_t digest_len, uint32_t key_type,  
+		  char *server_version_string, uint8_t service_type){
+
+
+    int name_len = strlen(dns_name);
+
+    int version_len = strlen(server_version_string);
+
+    int total_len = name_len +  digest_len 
+          + version_len + 12; // ip-addr, port, key_type, null-byte X 2 = 12
+    char * buf = (char*)malloc(total_len);
+    char *ptr = buf;
+    memcpy(ptr, dns_name, name_len + 1);
+    ptr += name_len + 1;
+    memcpy(ptr, server_version_string, version_len + 1);
+    ptr += version_len + 1;
+    memcpy(ptr, &ip_addr, 4);
+    ptr += 4;
+    memcpy(ptr, &port, 2);
+    ptr += 2;
+    memcpy(ptr, &key_type, 4);
+    ptr += 4;
+    memcpy(ptr, digest, digest_len);
+
+    int res = sendToUnixSock(FINISHED_CHILD_SOCK_NAME, buf, total_len); 
+  
+    if(res < 0) {
+        printf("Failed to report probe to recording socket for: \n"); 
+        printf("'%s:%d',%d = '%s' \n", dns_name, port, service_type,
+            buf_2_hexstr((char*)digest, digest_len)); 
+    }
+
+    free(buf);
+}
+
