4d89a948d8f48726a2bc8456acdad547f8744ceb
Ethan Jackson <ejj@andrew.cmu.edu>
2008-05-21 22:48:43 +0000

    Updated test policy fake to use the new printing output
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/multissh/notarycode@223 4bf60a73-bb1f-0410-bc46-cb087b644473

--- utilities/test_policy.c
+++ utilities/test_policy.c
@@ -42,23 +42,12 @@ int main(int argc, char **argv) {
   int max_stale_days = atoi(argv[6]); 
   int max_stale_secs = DAY2SEC(max_stale_days); 
 
-  /*
-  printf("service: %s\n"
-      "notary list: %s\n"
-      "test key: %s\n"
-      "key type: %s\n"
-      "Q: %d\n" 
-      "max stale days %d\n", 
-      argv[1], argv[2], argv[3], keytype_2_str(key_type), 
-      quorum_thresh, max_stale_days);*/
-
    SSHNotary *notary = init_ssh_notary();
    load_notary_server_file(notary, argv[2]); 
    fetch_notary_observations(notary, argv[1], 
        TIMEOUT, NUM_RETRIES);
 
 
-   printf("checking quorum \n");
    BOOL is_cur_consistent; 
    uint32_t duration = get_quorum_duration(notary, key_buf, KEY_LEN, key_type, 
        quorum_thresh, max_stale_secs, &is_cur_consistent);
--- utilities/test_policy_fake.c
+++ utilities/test_policy_fake.c
@@ -17,42 +17,38 @@
 unsigned int notary_debug = DEBUG_ERROR | DEBUG_POLICY;
 //unsigned int notary_debug = DEBUG_ERROR;
 
-int main(int argc, char **argv)
-{
-   
+int main(int argc, char **argv){
+
   if (argc != 6) { 
-      printf("Usage: <obs-file> <test-key> <key-type> <quorum-thresh> <max-stale-days>\n");
-      exit(1);
-   }
+    printf("Usage: <obs-file> <test-key> <key-type>"
+        " <quorum-thresh> <max-stale-days>\n");
+    exit(1);
+  }
 
   int key_type = str_2_keytype(argv[3]); 
-  printf("using %s test key = '%s' \n\n", keytype_2_str(key_type), argv[2]);
   char * key_buf = (char*)malloc(KEY_LEN); 
   int len_out = hexstr_2_buf(argv[2], key_buf, KEY_LEN); 
   if(len_out != KEY_LEN) {
-     printf("error, only read %d key bytes, expected %d \n",
-              len_out, KEY_LEN);
-      exit(1);
+    printf("error, only read %d key bytes, expected %d \n",
+        len_out, KEY_LEN);
+    exit(1);
   }
-  
+
   int quorum_thresh = atoi(argv[4]); 
   int max_stale_days = atoi(argv[5]); 
-  printf("using Q = %d  max-stale-days = %d\n", quorum_thresh, max_stale_days); 
   int max_stale_secs = DAY2SEC(max_stale_days); 
 
   Notary *notary = load_fake_replies_from_file(argv[1]); 
-  
+
+  BOOL is_cur_consistent; 
+  uint32_t duration = get_quorum_duration(notary, key_buf, KEY_LEN, key_type, 
+      quorum_thresh, max_stale_secs, &is_cur_consistent);
+
+  print_policy_results(notary, key_buf, KEY_LEN, key_type, quorum_thresh, 
+      max_stale_secs, is_cur_consistent, duration);
   print_notary_reply(stdout, notary); 
   printf("\n"); 
 
-   printf("checking quorum \n");
-   BOOL is_cur_consistent; 
-   uint32_t duration = get_quorum_duration(notary, key_buf, KEY_LEN, key_type, 
-                                   quorum_thresh, max_stale_secs, &is_cur_consistent);
-
-   printf("quorum-duration = %f  days (is_cur_consistent = %d) \n", 
-       SEC2DAY(duration), is_cur_consistent);
-
-   return 0;
+  return 0;
 }
 
