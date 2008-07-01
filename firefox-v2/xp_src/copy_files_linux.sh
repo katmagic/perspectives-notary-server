f7c24adfc02911757b7826422d72551600b192cf
Ethan Jackson <ejj@andrew.cmu.edu>
2008-07-01 17:53:02 +0000

    The scripts automatically copy files to the dist directory now.
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/multissh/notarycode@261 4bf60a73-bb1f-0410-bc46-cb087b644473

--- firefox-v2/xp_src/copy_files_linux.sh
+++ firefox-v2/xp_src/copy_files_linux.sh
@@ -3,6 +3,6 @@ PLATFORM=Linux_x86-gcc3
 LIB_FILE=Perspectives.so
 
 cp IPerspectives.xpt $PLUGIN_DIR/components
+cp IPerspectives.xpt ../dist/components
 cp $LIB_FILE $PLUGIN_DIR/platform/$PLATFORM/components
-
-
+cp $LIB_FILE ../dist/platform/$PLATFORM/components
--- firefox-v2/xp_src/copy_files_osx.sh
+++ firefox-v2/xp_src/copy_files_osx.sh
@@ -3,6 +3,8 @@ PLATFORM=Darwin_x86-gcc3
 LIB_FILE=Perspectives.dylib
 
 cp IPerspectives.xpt $PLUGIN_DIR/components
+cp IPerspectives.xpt ../dist/components
 cp $LIB_FILE $PLUGIN_DIR/platform/$PLATFORM/components
+cp $LIB_FILE ../dist/platform/$PLATFORM/components
 
 
