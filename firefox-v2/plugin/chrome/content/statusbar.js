9bf68e9853f8e733fbbe1385ab8e44ac4e02b0c3
Ethan Jackson <ejj@andrew.cmu.edu>
2008-08-20 01:18:12 +0000

    Screwed up this file on the last commit
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/multissh/notarycode@349 4bf60a73-bb1f-0410-bc46-cb087b644473

--- firefox-v2/plugin/chrome/content/statusbar.js
+++ firefox-v2/plugin/chrome/content/statusbar.js
@@ -1,10 +1,9 @@
 var root_prefs = Components.classes["@mozilla.org/preferences-service;1"]
 .getService(Components.interfaces.nsIPrefBranchInternal);
 
-function ShowInfo(ssl_cache) {
+function ShowInfo(ssl_cache,other_cache) {
   window.openDialog("chrome://perspectives/content/statusbar-info.xul", 
-      "perspectiveInfo", "centerscreen, chrome, toolbar", gBrowser,
-      ssl_cache).focus();
+      "perspectiveInfo", "centerscreen, chrome, toolbar", gBrowser, ssl_cache,other_cache).focus();
 }
 
 function evtLoad(evt){
