47b2063f80bab63100c3d5eede0620bc0e4ef450
Ethan Jackson <ejj@andrew.cmu.edu>
2008-08-20 01:07:26 +0000

    Added option not to show label
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/multissh/notarycode@347 4bf60a73-bb1f-0410-bc46-cb087b644473

--- firefox-v2/plugin/chrome/content/statusbar.js
+++ firefox-v2/plugin/chrome/content/statusbar.js
@@ -1,12 +1,18 @@
 var root_prefs = Components.classes["@mozilla.org/preferences-service;1"]
 .getService(Components.interfaces.nsIPrefBranchInternal);
 
-function ShowInfo(ssl_cache,other_cache) {
+function ShowInfo(ssl_cache) {
   window.openDialog("chrome://perspectives/content/statusbar-info.xul", 
-      "perspectiveInfo", "centerscreen, chrome, toolbar", gBrowser, ssl_cache,other_cache).focus();
+      "perspectiveInfo", "centerscreen, chrome, toolbar", gBrowser,
+      ssl_cache).focus();
 }
 
 function evtLoad(evt){
   initNotaries();
+  if(!root_prefs.getBoolPref("perspectives.show_label")){
+    document.getElementById("perspective-statusbar-label").hidden =
+      true;
+  }
 }
+
 window.addEventListener('load',evtLoad,false);
