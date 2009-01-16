de5efe3515bc68d539d08c8f2da03f6fe3058f19
Dan Wendlandt <danwent@gmail.com>
2009-01-16 08:51:19 +0000

    improvements to the python scanner to fix file limit issue, and improve logging.  adding script to run scan from cron using list_by_last_obs.py and simple_probe.py
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/multissh/notarycode@397 4bf60a73-bb1f-0410-bc46-cb087b644473

---
+++ scripts/run_scan2.sh
@@ -0,0 +1,66 @@
+#!/usr/local/bin/bash
+
+
+function usage {
+
+    echo "usage: db <scann-config-file> <max-simultaneous> <timeout>"
+    echo "usage: f <service-list-file> <max-simultaneous> <timeout>"
+    exit 1
+}
+
+if [ $# != 4 ] && [ $# != 5 ] ; then 
+  usage;  
+fi 
+
+if [ $# = 5 ] ; then
+  cd $5
+fi
+
+if ! [ -d "bin" ] ; then 
+  echo "must be invoked from the top level of the notary package"
+  exit 1
+fi
+
+if ! [ -d "log" ] ; then 
+  mkdir log
+fi
+
+time=`date +%s`
+
+
+if [ $1 = "db" ]; then
+
+  if ! [ -f $2 ] ; then 
+    echo "sorry, config file '$2' doesn't exist"
+    exit 1 
+  fi
+  db_env_fname=$(cat $2 | grep db_env_fname | awk -F "=" '{print $2}') 
+  db_fname=$(cat $2 | grep db_fname | awk -F "=" '{print $2}') 
+
+  if ! [ -d "$db_env_fname" ]; then
+    echo "ERROR: cannot find DB environment dir '$db_env_fname'"
+    exit 1
+  fi
+
+  if ! [ -f "$db_env_fname/$db_fname" ]; then
+    echo "ERROR: cannot find DB file '$db_env_fname/$db_fname'"
+    exit 1
+  fi
+  
+  file=log/scanned_hosts_$time.txt
+
+  python scripts/list_by_last_obs.py $db_env_fname $db_fname older 0 | awk '{print $1}' > $file
+
+
+elif [ "$1" = "f" ]; then
+  if ! [ -f $2 ]; then
+    echo "Error: couldn't locate input file '$2'"
+    usage; 
+  fi 
+  file=$2
+else 
+  usage; 
+fi
+
+python scripts/simple_probe.py $file $3 $4 >> log/scan_results.log 2>&1
+
--- scripts/simple_probe.py
+++ scripts/simple_probe.py
@@ -1,52 +1,76 @@
 import os
 import sys
-from subprocess import * 
+from subprocess import Popen,PIPE,STDOUT 
 import time 
 
-# an extremely simple scanner with no parallelism.
-# this means that if any of the services listed time out,
-# everything will hang on the order of 30 seconds.  Right 
-# now I use this scanner only on hosts that have not been
-# successfully reached by the normal scanning process.  
-
+# a simple parallel scanner that implements timeouts.
+# note: this scanner must be run in tandem with the 'real'
+# C scanner, as the 'ssl_scan' and 'ssh_scan' utilities
+# report their values back to that scanner via a unix 
+# domain socket.  The reason for this scanner is that there
+# seems to be issues probe requests getting lost/timedout
+# when requested by the c-scanner. 
 
 def start_probe(sid): 
   host_and_port, service_type = sid.split(",")
   if service_type == "2": 
-    cmd = "bin/ssl_scan s_client -connect %s" % host_and_port
+    cmd = [ "bin/ssl_scan", "s_client", "-connect",  host_and_port ]
   elif service_type == "1": 
-    cmd = "bin/ssh_scan %s" % host_and_port
+    cmd = [ "bin/ssh_scan",  host_and_port ]
   else: 
     print "invalid service_type for '%s'" % sid
     return  
-  return Popen(cmd, stdout=PIPE, stderr=STDOUT, shell=True)
+  return Popen(cmd, stdout=PIPE, stderr=STDOUT, shell=False)
    
-
-if len(sys.argv) != 3: 
-  print "usage: <service_id_file> <max simultaneous>"
-  exit(1)
+if len(sys.argv) != 4: 
+  print "usage: <service_id_file> <max simultaneous> <timeout sec> "
+  sys.exit(1)
 
 f = open(sys.argv[1])
 to_probe = [ line.rstrip() for line in f ] 
 
+total_count = len(to_probe)
 active_sids = {}
 done = False
 max_sim = int(sys.argv[2])
+timeout_sec = int(sys.argv[3]) 
+failure_count = 0
+start_time = time.time()
+print "*** Starting scan of %s services at %s" % \
+    (total_count,time.ctime())
+print "*** Timeout = %s sec  Max-Simultaneous = %s" % \
+    (timeout_sec, max_sim) 
 
-while not done:
+while True:
   while( len(active_sids) < max_sim ): 
-    if (len(to_probe) == 0):
+    l = len(to_probe)
+    if (l == 0):
       break
+    if (l % 1000 == 0): 
+      print "%s probes remaining" % l
     sid = to_probe.pop()
-    active_sids[sid] = start_probe(sid)
+    active_sids[sid] = (start_probe(sid), time.time()) 
 
   if(len(active_sids) == 0): 
     break # all done
     
-  for sid,p in active_sids.items():
-    if p.poll() != None:
-      code = p.poll()
+  now = time.time() 
+  for sid,(p,t) in active_sids.items():
+    code = p.poll()
+    if code != None:
       if code != 8: 
-        print "'%s' exited with error code '%s'" % (sid,code)
+        print "failed: %s %s" % (sid,code)
+        failure_count += 1
+      p.wait() # apparently this is needed on FreeBSD?
       del active_sids[sid]
+    else:
+      run_time = now - t
+      if run_time > timeout_sec:
+        os.kill(p.pid,9) # p.kill() required python 2.6
+  sys.stdout.flush()
+  sys.stderr.flush()
   time.sleep(1)
+
+duration = time.time() - start_time
+print "*** Finished scan at %s. Scan took %s seconds" % (time.ctime(), duration) 
+print "*** %s of %s probes failed" % (failure_count, total_count)
