e555da94e0888806c38a090a3d41f515b573c0d8
Dan Wendlandt <danwent@gmail.com>
2009-03-23 02:48:33 +0000

    make non-windows mozilla client accept RSA-SHA256 signatures as well as RSA-MD5
    git-svn-id: https://moo.cmcl.cs.cmu.edu/svn/multissh/notarycode@406 4bf60a73-bb1f-0410-bc46-cb087b644473

--- firefox-v2/xp_src/moz_crypto.cpp
+++ firefox-v2/xp_src/moz_crypto.cpp
@@ -34,28 +34,16 @@
 #include "secder.h"
 #include "common.h"
 
-
-PRBool verify_signature(char *buf, int msg_len, char *server_key) { 
+// verifies a signature using the XPCOM 'datasignatureverifier' component.
+// since the signature of the notary does not contain the full DER header
+// expected by the datasignatureverifier, we also pass in a DER header
+// to be prepended to the notary signature.  This DER header also indicates
+// the exact signature algorithm (e.g., RSA with MD5) 
+PRBool verify_signature_with_header(char *der_header, int der_header_len, 
+				 unsigned char* sig, int sig_len,
+				 unsigned char* data, int data_len, 
+				 char *server_key) { 
     nsresult rv; 
-    //NOTE: the mozilla signature verification API expects a DER-encoded
-    // object that includes information about the signature algorithms
-    // used.  This data is not in the openssl generated signature, which
-    // is only the signature itself.  This binary array data is the 
-    // correct header for the signature algorithm and size that we 
-    // currently use: MD5 has with RSA signature, and a length of 172 bytes.
-    // If the size of key used by notaries changes, we'll have to change 
-    // this as well.  The size is encoded in two places, 0xbf indicates the
-    // overall size of the entire DER structure, and 0xad indicates the 
-    // size of the DER signature.  
-    char der_header[22] = { 0x30, 0x81, 0xbf, 0x30, 0x0d, 0x06, 0x09, 
-	0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x04, 0x05, 
-	0x00, 0x03, 0x81, 0xad, 0x00 };
-	notary_header *hdr = (notary_header*)buf;
-    //FIXME: security issue with taking sig len from outside?  
-    int sig_len = SIGNATURE_LEN; 
-    int data_len = ntohs(hdr->total_len) - sig_len - sizeof(notary_header);
-    unsigned char* data = (unsigned char*)(hdr + 1);
-    unsigned char *sig = (unsigned char*) (data + data_len);
     unsigned char *full_sig = NULL;
     char *sig64 = NULL; 
     int b64_len = 0; 
@@ -69,16 +57,14 @@ PRBool verify_signature(char *buf, int msg_len, char *server_key) {
       DPRINTF(DEBUG_ERROR, "failed to load c++ datasignatureverifier\n"); 
       return PR_FALSE;  
     }
-    DPRINTF(DEBUG_INFO, "msg verify: data_len = %d sig_len = %d \n",
-			data_len, sig_len);
 
-    full_sig = (unsigned char*) malloc(sig_len + 22); 
-    memcpy(full_sig, der_header, 22); 
-    memcpy(full_sig + 22, sig, sig_len);  
+    full_sig = (unsigned char*) malloc(sig_len + der_header_len); 
+    memcpy(full_sig, der_header, der_header_len); 
+    memcpy(full_sig + der_header_len, sig, sig_len);  
     SECItem sig_item;
     sig_item.type = siBuffer;
     sig_item.data = full_sig;
-    sig_item.len = sig_len + 22; 
+    sig_item.len = sig_len + der_header_len; 
    
     b64_len = 2 * sig_item.len;  
     sig64 = (char*)malloc(b64_len); 
@@ -104,4 +90,51 @@ PRBool verify_signature(char *buf, int msg_len, char *server_key) {
 	if(sig64) free(sig64); 
 	return PR_FALSE; 
 } 
+
+
+#define DER_HEADER_LEN 22
+
+PRBool verify_signature(char *buf, int msg_len, char *server_key) { 
+    PRBool res; 
+    
+    //NOTE: the mozilla signature verification API expects a DER-encoded
+    // object that includes information about the signature algorithms
+    // used.  This data is not in the openssl generated signature, which
+    // is only the signature itself.  This binary array data is the 
+    // correct header for the signature algorithm and size that we 
+    // currently use: MD5 has with RSA signature, and a length of 172 bytes.
+    // If the size of key used by notaries changes, we'll have to change 
+    // this as well.  The size is encoded in two places, 0xbf indicates the
+    // overall size of the entire DER structure, and 0xad indicates the 
+    // size of the DER signature.  
+    char der_header_md5[DER_HEADER_LEN] = {    0x30, 0x81, 0xbf, 
+	0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 
+	0x0d, 0x01, 0x01, 0x04, 0x05, 0x00, 0x03, 0x81, 0xad, 0x00 };
+    char der_header_sha256[DER_HEADER_LEN] = { 0x30, 0x81, 0xbf, 
+	0x30, 0x0a, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 
+	0x0a, 0x01, 0x01, 0x0b, 0x05, 0x00, 0x03, 0x81, 0xad, 0x00 };
+    int sig_len = SIGNATURE_LEN;  
+    notary_header *hdr = (notary_header*)buf;
+    int data_len = ntohs(hdr->total_len) - sig_len - sizeof(notary_header);
+    unsigned char* data = (unsigned char*)(hdr + 1);
+    unsigned char *sig = (unsigned char*) (data + data_len);
+    DPRINTF(DEBUG_INFO, "msg verify: data_len = %d sig_len = %d \n",
+			data_len, sig_len);
+    res = verify_signature_with_header(der_header_sha256, DER_HEADER_LEN, sig, 
+					sig_len, data, data_len, 
+					server_key);
+    if(res) { 
+      DPRINTF(DEBUG_ERROR, "SHA256 verification succeeded\n"); 
+      return res; 
+    }
+    DPRINTF(DEBUG_ERROR, "SHA256 verification failed\n"); 
+    res = verify_signature_with_header(der_header_md5, DER_HEADER_LEN, sig, 
+					sig_len, data, data_len, 
+					server_key);
+    if(!res) { 
+      DPRINTF(DEBUG_ERROR, "MD5 verification failed\n"); 
+    }
+    DPRINTF(DEBUG_ERROR, "MD5 verification succeeded\n"); 
+    return res; 
+}
 
