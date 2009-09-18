4240753115b62c57b7aee332c7bbbbee30c804a1
Ciprian, Dorin Craciun <ciprian@volution.ro>
2009-09-18 16:48:09 +0300

    Added SysV init.d control-like script.

---
+++ scripts/default_control.sh
@@ -0,0 +1,44 @@
+#!@bash_EXEC@
+
+exec >&2
+
+set -e -u -o pipefail || exit 1
+
+usage () {
+	echo "usage: start | stop | restart | check | maintenance"
+	exit 1
+}
+
+if [ "$#" != 1 ] ; then
+	usage
+fi
+
+case "${1}" in
+	
+	( start )
+		exec @notary_bin_PATH@/@notary_bin_PREFIX@default_start_daemons
+	;;
+	
+	( stop )
+		exec @notary_bin_PATH@/@notary_bin_PREFIX@default_stop_daemons
+	;;
+	
+	( restart )
+		@notary_bin_PATH@/@notary_bin_PREFIX@default_stop_daemons || true
+		exec @notary_bin_PATH@/@notary_bin_PREFIX@default_start_daemons
+	;;
+	
+	( check )
+		exec @notary_bin_PATH@/@notary_bin_PREFIX@default_check_daemons
+	;;
+	
+	( maintenance )
+		exec @notary_bin_PATH@/@notary_bin_PREFIX@default_maintenance
+	;;
+	
+	( * )
+		usage
+	;;
+esac
+
+exit 1
